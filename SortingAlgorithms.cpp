c652f381669dd67b1495d3c01f063c1a9c5569e5
Kiss Áron <aron123dev@gmail.com>
2023-12-30 19:53:15 +0100

    Add parallel sorting functions

--- SortingAlgorithms.cpp
+++ SortingAlgorithms.cpp
@@ -5,58 +5,155 @@
 
 void SortingAlgorithms::bucketSort(std::vector<std::byte>& vector)
 {
-    const auto n = vector.size();
-    const int M = static_cast<int>(*std::ranges::max_element(vector)) + 1;
-    
-    std::vector<std::vector<std::byte>> buckets(n);
-    
-    for (size_t i = 0; i < n; ++i) {
-	    const int bucketIndex = static_cast<int>(std::floor(static_cast<float>(n) * static_cast<float>(vector[i]) / static_cast<float>(M)));
-        buckets[bucketIndex].push_back(vector[i]);
-    }
-    
-    for (auto& bucket : buckets) {
-        std::ranges::sort(bucket);
-    }
-    
-    int index = 0;
-    for (auto& bucket : buckets) {
-        for (const auto value : bucket) {
-            vector[index++] = value;
-        }
-    }
+	const auto n = vector.size();
+	const int M = static_cast<int>(*std::ranges::max_element(vector)) + 1;
+
+	std::vector<std::vector<std::byte>> buckets(n);
+
+	for (size_t i = 0; i < n; ++i)
+	{
+		const int bucketIndex = static_cast<int>(std::floor(static_cast<float>(n) * static_cast<float>(vector[i]) / static_cast<float>(M)));
+		buckets[bucketIndex].push_back(vector[i]);
+	}
+
+	for (auto& bucket : buckets)
+	{
+		std::ranges::sort(bucket);
+	}
+
+	int index = 0;
+	for (auto& bucket : buckets)
+	{
+		for (const auto value : bucket)
+		{
+			vector[index++] = value;
+		}
+	}
 }
 
-void SortingAlgorithms::quickSort(std::vector<std::byte>& vector)
+void SortingAlgorithms::parallelBucketSort(std::vector<std::byte>& vector)
 {
-    if (!vector.empty()) {
-        quickSortWith(vector, 0, vector.size() - 1);
-    }
+	const auto n = static_cast<int64_t>(vector.size());
+	const int M = static_cast<int>(*std::ranges::max_element(vector)) + 1;
+
+	std::vector<std::vector<std::byte>> buckets(n);
+
+#pragma omp parallel for  // NOLINT(clang-diagnostic-source-uses-openmp)
+	for (int64_t i = 0; i < n; ++i)
+	{
+		const int bucketIndex = static_cast<int>(std::floor(static_cast<float>(n) * static_cast<float>(vector[i]) / static_cast<float>(M)));
+#pragma omp critical
+		buckets[bucketIndex].push_back(vector[i]);
+	}
+
+#pragma omp parallel for
+	for (int64_t i = 0; i < n; ++i) // NOLINT(modernize-loop-convert)
+	{  
+		std::ranges::sort(buckets[i]);
+	}
+
+	int index = 0;
+#pragma omp parallel for
+	for (int64_t i = 0; i < n; ++i)  // NOLINT(modernize-loop-convert)
+	{
+		for (const auto value : buckets[i])
+		{
+			vector[index++] = value;
+		}
+	}
 }
 
 size_t SortingAlgorithms::partition(std::vector<std::byte>& vec, size_t low, size_t high) {
 	const auto pivot = vec[high];
-    size_t i = low - 1;
+	size_t i = low - 1;
 
-    for (size_t j = low; j < high; ++j) {
-        if (vec[j] < pivot) {
-            ++i;
-            std::swap(vec[i], vec[j]);
-        }
-    }
+	for (size_t j = low; j < high; ++j)
+	{
+		if (vec[j] < pivot)
+		{
+			++i;
+			std::swap(vec[i], vec[j]);
+		}
+	}
 
-    std::swap(vec[i + 1], vec[high]);
-    return i + 1;
+	std::swap(vec[i + 1], vec[high]);
+	return i + 1;
 }
 
 void SortingAlgorithms::quickSortWith(std::vector<std::byte>& vec, size_t low, size_t high) {
-    if (low < high) {
-	    const size_t pivotIndex = partition(vec, low, high);
+	if (low < high)
+	{
+		const size_t pivotIndex = partition(vec, low, high);
 
-        if (pivotIndex > 0) {
-            quickSortWith(vec, low, pivotIndex - 1);
-        }
+		if (pivotIndex > 0)
+		{
+			quickSortWith(vec, low, pivotIndex - 1);
+		}
 
-        quickSortWith(vec, pivotIndex + 1, high);
-    }
+		quickSortWith(vec, pivotIndex + 1, high);
+	}
+}
+
+void SortingAlgorithms::quickSort(std::vector<std::byte>& vector)
+{
+	if (!vector.empty())
+	{
+		quickSortWith(vector, 0, vector.size() - 1);
+	}
+}
+
+int64_t SortingAlgorithms::partition(std::vector<std::byte>& vec, int64_t low, int64_t high) {
+	const auto pivot = vec[high];
+	auto i = low - 1;
+
+#pragma omp parallel for
+	for (int64_t j = low; j < high; ++j)
+	{
+		if (vec[j] < pivot)
+		{
+#pragma omp critical
+			{
+				++i;
+				std::swap(vec[i], vec[j]);
+			}
+		}
+	}
+
+	std::swap(vec[i + 1], vec[high]);
+	return i + 1;
+}
+
+void SortingAlgorithms::quickSortWith(std::vector<std::byte>& vec, int64_t low, int64_t high) {
+	if (low < high)
+	{
+		const auto pivotIndex = partition(vec, low, high);
+
+#pragma omp parallel sections
+		{
+#pragma omp section
+			{
+				if (pivotIndex > 0)
+				{
+					quickSortWith(vec, low, pivotIndex - 1);
+				}
+			}
+
+#pragma omp section
+			{
+				quickSortWith(vec, pivotIndex + 1, high);
+			}
+		}
+	}
+}
+
+void SortingAlgorithms::parallelQuickSort(std::vector<std::byte>& vector)
+{
+	if (!vector.empty())
+	{
+#pragma omp parallel
+		{
+#pragma omp single nowait
+			quickSortWith(vector, 0, vector.size() - 1);
+		}
+	}
 }
--- SortingAlgorithms.h
+++ SortingAlgorithms.h
@@ -5,10 +5,16 @@ class SortingAlgorithms
 {
 public:
 	static void bucketSort(std::vector<std::byte>& vector);
+	static void parallelBucketSort(std::vector<std::byte>& vector);
+
 	static void quickSort(std::vector<std::byte>& vector);
+	static void parallelQuickSort(std::vector<std::byte>& vector);
 
 private:
 	static size_t partition(std::vector<std::byte>& vec, size_t low, size_t high);
 	static void quickSortWith(std::vector<std::byte>& vec, size_t low, size_t high);
+
+	static int64_t partition(std::vector<std::byte>& vec, int64_t low, int64_t high);
+	static void quickSortWith(std::vector<std::byte>& vec, int64_t low, int64_t high);
 };
 
